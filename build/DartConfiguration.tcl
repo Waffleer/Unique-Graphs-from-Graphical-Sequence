978a8863f3526863007d02e5d8f46bbc3cb83eb1
Nicholas Doboszenski <waffleer@orangeSlicer.>
2025-12-09 19:49:52 -0600

    Finished row implimentation

--- build/DartConfiguration.tcl
+++ build/DartConfiguration.tcl
@@ -11,7 +11,7 @@ BuildDirectory: /home/waffleer/Unique-Graphs-from-Graphical-Sequence/build
 CostDataFile: 
 
 # Site is something like machine.domain, i.e. pragmatic.crd
-Site: tandemaus
+Site: orangeSlicer
 
 # Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
 BuildName: Linux-g++
--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include "Sequence_parser.h"
 #include "Row.h"
+#include <vector>
 
 int main(int, char**){
+    std::vector<int> init = {3, 3, 2, 2, 2};
+    Row root(init);
+    Row l1(&root);
+    
     std::cout << "Hello, from unique_graphs_from_sequence!\n";
 }
